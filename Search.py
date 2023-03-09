e944a567a2e6e9ec83fbba4d447c6e5f46cf324c
Carlos <carloszaragozabeato15@gmail.com>
2023-03-09 08:51:47 +0100

    Segundo Super

--- Search.py
+++ Search.py
@@ -13,9 +13,7 @@ class Search:
 
 
 
-
-
-
+    #BUSCAR EN CARREFOUR
     def CarrefourProduct(self):
         self.driver.get('https://www.carrefour.es/?gclid=Cj0KCQiAgaGgBhC8ARIsAAAyLfHJZAkh3PZU6zJ6jhoAhxnOvsCcPahmWyNHF4xUuQiT2F9gBk3rdloaAkgGEALw_wcB&gclsrc=aw.ds')
         
@@ -38,10 +36,33 @@ class Search:
 
         #Cojemos la informacion del producto
         price = self.driver.find_element(By.XPATH, '//*[@id="ebx-grid"]/article[1]/div/p/strong')
-
-
-
-
-
         return ('Carrefour',price.text[: -1].replace(' ', ''))
+
+
+
+
+
+    #BUSCAR EN AHORRA MAS
+    def AhorraMasProduct(self):
+        self.driver.get('https://www.ahorramas.com/')
+
+        time.sleep(1)
+        
+        btn_cookies = self.driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
+        btn_cookies.click()
+
+        #Introducimos el producto
+        #Accedo a traves de la clase porque introduciendo el XPATH no tenia acceso a el.
+        input_text = self.driver.find_element(By.CLASS_NAME, 'search-field') 
+        input_text.click()
+        input_text.send_keys(self.product_name)
+
+        search_button = self.driver.find_element(By.CLASS_NAME, 'fa-search')
+        search_button.click()
+
+        price = self.driver.find_element(By.XPATH, '//*[@id="product-search-results"]/div[2]/div[4]/div[2]/div[1]/div/div/div[3]/div[2]/div[1]/div/span/span')
+
+        return ('Ahorra Mas',price.text[: -1].replace(' ', ''))
+
+
 
